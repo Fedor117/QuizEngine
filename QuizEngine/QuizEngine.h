b551662e765265eca3c3935cccd023f429b583b4
Fedor Valiavko <fedor.valiavko@gmail.com>
2020-06-30 14:53:49 +0100

    Initial Commit

---
+++ QuizEngine/QuizEngine.h
@@ -0,0 +1,19 @@
+//
+//  QuizEngine.h
+//  QuizEngine
+//
+//  Created by Anna Rasina on 6/30/20.
+//  Copyright © 2020 Fedor Valiavko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuizEngine.
+FOUNDATION_EXPORT double QuizEngineVersionNumber;
+
+//! Project version string for QuizEngine.
+FOUNDATION_EXPORT const unsigned char QuizEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizEngine/PublicHeader.h>
+
+
